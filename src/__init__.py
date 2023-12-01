e1c099129203009ff557c1ed891c4c1070914df2
shifeng <a245241964@gmail.com>
2023-12-01 18:37:33 -0500

    part 6-14

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@
 import os
 from flask import Flask  # new
 from flask_sqlalchemy import SQLAlchemy
+
 # instantiate the db
 db = SQLAlchemy()
 # new
@@ -19,7 +20,8 @@ def create_app(script_info=None):
     from src.api.ping import ping_blueprint
     app.register_blueprint(ping_blueprint)
     # shell context for flask cli
-
+    from src.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
     @app.shell_context_processor
     def ctx():
         return {'app': app, 'db': db}
--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,7 +1,7 @@
 # src/tests/conftest.py
 import pytest
 from src import create_app, db  # updated
-
+from src.api.models import User
 
 @pytest.fixture(scope='module')
 def test_app():
@@ -17,3 +17,12 @@ def test_database():
     yield db  # testing happens here
     db.session.remove()
     db.drop_all()
+
+@pytest.fixture(scope='function')
+def add_user():
+    def _add_user(username, email):
+        user = User(username=username, email=email)
+        db.session.add(user)
+        db.session.commit()
+        return user
+    return _add_user
